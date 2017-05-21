afd7058405a54e35d6f06c5747fa60a14a5f3d15
mikong <michaelgalero@gmail.com>
2017-05-21 19:47:06 +0800

    Login: Display alert message. Fix layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def alert_classes(flash_name)
+    classes = ['alert']
+    case flash_name
+    when 'notice'
+      classes << 'alert-success'
+    when 'alert'
+      classes << 'alert-warning'
+    when 'error'
+      classes << 'alert-danger'
+    else
+      classes << 'alert-info'
+    end
+    classes
+  end
+
 end
