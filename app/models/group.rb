a10c3a1247bb29f3d20b6b856aa718fada8313da
mikong <michaelgalero@gmail.com>
2017-05-25 00:28:59 +0800

    Group: seed data, validation, and color field prefix.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   validates :name, presence: true
+  validates :color, format: {
+    with: /\A([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/,
+    message: 'is not a valid color hex' }
 end
