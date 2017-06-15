fd34ac044e67f8bf7eb921f87f7068b8ad0e8f88
mikong <michaelgalero@gmail.com>
2017-06-16 00:06:09 +0800

    Set default emoji. Require emoji.

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,13 @@
 class Announcement < ApplicationRecord
-  validates :body, presence: true
+  validates :body, :emoji, presence: true
+
+  after_initialize :set_default_emoji
+
+  DEFAULT_EMOJI = ':octopus:'
+
+private
+  def set_default_emoji
+    self.emoji ||= DEFAULT_EMOJI
+  end
+
 end
